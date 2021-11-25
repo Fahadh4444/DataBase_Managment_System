23875ce50c7cd9fce42d01d2711af10fe9e8ca4e
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-11-25 23:45:47 +0530

    Uploaded server.js for backend in SSS.

---
+++ Projects/SSS/backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config(); // ALLOWS ENVIRONMENT VARIABLES TO BE SET ON PROCESS.ENV SHOULD BE AT TOP
+
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+//* Middleware
+app.use(express.json()); // parse json bodies in the request object
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//* Routes
+app.use("/auth", require("./routes/authRoutes"));
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  console.log(err.name);
+  console.log(err.code);
+
+  res.status(500).json({
+    message: "Something went rely wrong",
+  });
+});
+
+// Listen on pc port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
