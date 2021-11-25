08a0dc87679c3c42ab0368bbe31c483e1a5b2d18
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-11-25 23:46:56 +0530

    Uploaded MySQL database connection configuration.

---
+++ Projects/SSS/backend/config/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config('../.env');
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD
+    host: 'localhost',
+    user: 'root',
+    database: 'sss',
+    password: '1691',
+});
+
+// let sql = "SELECT * FROM users;";
+
+// pool.execute(sql, function(err, result){
+//     if(err) throw err;
+//     result.forEach(res => {
+//         console.log(res)
+//     });
+// });
+
+module.exports = pool.promise();
