94b5b229e4a8a66d77e39e00cb97a112846c41e6
Fahadh Kasala <mdfahadh2000@gmail.com>
2021-12-03 14:05:33 +0530

    Uploaded about page source codes.

---
+++ Projects/SSS/frontend/homepage/about/script.js
@@ -0,0 +1,297 @@
+function test(){
+	var tabsNewAnim = $('#navbarSupportedContent');
+	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+	var activeItemNewAnim = tabsNewAnim.find('.active');
+	var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+	var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+	var itemPosNewAnimTop = activeItemNewAnim.position();
+	var itemPosNewAnimLeft = activeItemNewAnim.position();
+	$(".hori-selector").css({
+		"top":itemPosNewAnimTop.top + "px", 
+		"left":itemPosNewAnimLeft.left + "px",
+		"height": activeWidthNewAnimHeight + "px",
+		"width": activeWidthNewAnimWidth + "px"
+	});
+	$("#navbarSupportedContent").on("click","li",function(e){
+		$('#navbarSupportedContent ul li').removeClass("active");
+		$(this).addClass('active');
+		var activeWidthNewAnimHeight = $(this).innerHeight();
+		var activeWidthNewAnimWidth = $(this).innerWidth();
+		var itemPosNewAnimTop = $(this).position();
+		var itemPosNewAnimLeft = $(this).position();
+		$(".hori-selector").css({
+			"top":itemPosNewAnimTop.top + "px", 
+			"left":itemPosNewAnimLeft.left + "px",
+			"height": activeWidthNewAnimHeight + "px",
+			"width": activeWidthNewAnimWidth + "px"
+		});
+	});
+}
+$(document).ready(function(){
+	setTimeout(function(){ test(); });
+});
+$(window).on('resize', function(){
+	setTimeout(function(){ test(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+	$(".navbar-collapse").slideToggle(300);
+	setTimeout(function(){ test(); });
+});
+
+
+
+// --------------add active class-on another-page move----------
+jQuery(document).ready(function($){
+	// Get current path and find target link
+	var path = window.location.pathname.split("/").pop();
+
+	// Account for home page with empty path
+	if ( path == '' ) {
+		path = 'index.html';
+	}
+
+	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
+	// Add active class to target link
+	target.parent().addClass('active');
+});
+
+
+/*MY FIRST JAVASCRIPT PROJECT not changing it if it works it works right :D*/
+var showText = function (target, message, index, interval) {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+}
+$(function () {
+
+  showText("#msg", "Terminal", 0, 100);
+  showText("#msg1", "My portfolio, type help", 0, 100);
+});
+ $(document).ready(function () {
+            setTimeout("$('#fname').focus();", 500);
+});
+
+/*var answered = document.getElementById('fname').value;
+alert("ÿou answered" + answered);
+var answer = function () {
+    switch(answered){
+        case "help":
+          alert("typed help!");
+        break;
+        default:
+          alert("urhm");
+    }
+}*/
+
+function checkforblank(){
+  if (document.getElementById('fname').value == "help") {
+    
+    showText("#msg2", 'commands:', 0, 100);
+    showText("#msg3", '#1 MAIN, back to MAIN menu', 0, 100);
+    showText("#msg4", '#2 PORT, nav to PORT page', 0, 100);
+    showText("#msg5", '#3 ABOUT, nav to ABOUT page', 0, 100);
+    showText("#msg6", '#4 CONT, nav to CONTACT page', 0, 100);
+    showText("#msg7", '#5 STOP, stop all', 0, 100);
+    return false; 
+  }
+  else{
+    alert('error: "help", dont have space at the end');
+    return false;
+  }
+
+}
+
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+             
+function autosize(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:0; padding:0';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
+}
+$(function() {
+    $('textarea').on('keypress', function(e) {
+        if (e.which == 32)
+            return false;
+    });
+});
+setTimeout(function(){
+   $("textarea").text("LOADING TERMINAL");
+},500); // 3 second delay
+
+setTimeout(function(){
+   $("textarea").text("STAND BY");
+},2500); // 3 second delay
+
+
+setTimeout(function(){
+   $("textarea").text("SUCCESSFUL");
+},5000); // 3 second delay
+
+
+setTimeout(function(){
+   $("textarea").text("");
+},7000); // 3 second delay
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// THE UNIVERSE. Code adapted from https://codepen.io/Acuetouag/pen/vOgmza
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+var starDensity = .216;
+var speedCoeff = .05;
+var width;
+var height;
+var starCount;
+var circleRadius;
+var circleCenter;
+var first = true;
+var giantColor = '180,184,240';
+var starColor = '226,225,142';
+var cometColor = '226,225,224';
+var canva = document.getElementById('universe');
+var stars = [];
+
+windowResizeHandler();
+window.addEventListener('resize', windowResizeHandler, false);
+
+createUniverse();
+
+function createUniverse() {
+  universe = canva.getContext('2d');
+
+  for (var i = 0; i < starCount; i++) {
+    stars[i] = new Star();
+    stars[i].reset();
+  }
+
+  draw();
+}
+
+function draw() {
+  universe.clearRect(0, 0, width, height);
+
+  var starsLength = stars.length;
+
+  for (var i = 0; i < starsLength; i++) {
+    var star = stars[i];
+    star.move();
+    star.fadeIn();
+    star.fadeOut();
+    star.draw();
+  }
+
+  window.requestAnimationFrame(draw);
+}
+
+function Star() {
+
+  this.reset = function() {
+    this.giant = getProbability(3);
+    this.comet = this.giant || first ? false : getProbability(10);
+    this.x = getRandInterval(0, width - 10);
+    this.y = getRandInterval(0, height);
+    this.r = getRandInterval(1.1, 2.6);
+    this.dx = getRandInterval(speedCoeff, 6 * speedCoeff) + (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120) + speedCoeff * 2;
+    this.dy = -getRandInterval(speedCoeff, 6 * speedCoeff) - (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120);
+    this.fadingOut = null;
+    this.fadingIn = true;
+    this.opacity = 0;
+    this.opacityTresh = getRandInterval(.2, 1 - (this.comet + 1 - 1) * .4);
+    this.do = getRandInterval(0.0005, 0.002) + (this.comet + 1 - 1) * .001;
+  };
+
+  this.fadeIn = function() {
+    if (this.fadingIn) {
+      this.fadingIn = this.opacity > this.opacityTresh ? false : true;
+      this.opacity += this.do;
+    }
+  };
+
+  this.fadeOut = function() {
+    if (this.fadingOut) {
+      this.fadingOut = this.opacity < 0 ? false : true;
+      this.opacity -= this.do / 2;
+      if (this.x > width || this.y < 0) {
+        this.fadingOut = false;
+        this.reset();
+      }
+    }
+  };
+
+  this.draw = function() {
+    universe.beginPath();
+
+    if (this.giant) {
+      universe.fillStyle = 'rgba(' + giantColor + ',' + this.opacity + ')';
+      universe.arc(this.x, this.y, 2, 0, 2 * Math.PI, false);
+    } else if (this.comet) {
+      universe.fillStyle = 'rgba(' + cometColor + ',' + this.opacity + ')';
+      universe.arc(this.x, this.y, 1.5, 0, 2 * Math.PI, false);
+
+      //comet tail
+      for (var i = 0; i < 30; i++) {
+        universe.fillStyle = 'rgba(' + cometColor + ',' + (this.opacity - (this.opacity / 20) * i) + ')';
+        universe.rect(this.x - this.dx / 4 * i, this.y - this.dy / 4 * i - 2, 2, 2);
+        universe.fill();
+      }
+    } else {
+      universe.fillStyle = 'rgba(' + starColor + ',' + this.opacity + ')';
+      universe.rect(this.x, this.y, this.r, this.r);
+    }
+
+    universe.closePath();
+    universe.fill();
+  };
+
+  this.move = function() {
+    this.x += this.dx;
+    this.y += this.dy;
+    if (this.fadingOut === false) {
+      this.reset();
+    }
+    if (this.x > width - (width / 4) || this.y < 0) {
+      this.fadingOut = true;
+    }
+  };
+
+  (function() {
+    setTimeout(function() {
+      first = false;
+    }, 50)
+  })()
+}
+
+function getProbability(percents) {
+  return ((Math.floor(Math.random() * 1000) + 1) < percents * 10);
+}
+
+function getRandInterval(min, max) {
+  return (Math.random() * (max - min) + min);
+}
+
+function windowResizeHandler() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  starCount = width * starDensity;
+  circleRadius = (width > height ? height / 2 : width / 2);
+  circleCenter = {
+    x: width / 2,
+    y: height / 2
+  }
+
+  canva.setAttribute('width', width);
+  canva.setAttribute('height', height);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
